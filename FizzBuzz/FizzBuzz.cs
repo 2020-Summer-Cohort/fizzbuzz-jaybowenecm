786b47dd205d417cd85bdb47fb58f3a5bc8bec30
Jay Bowen <jay@bowenecmsolutions.com>
2020-05-20 14:14:42 -0400

    iteration one

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz
+    {
+
+        public string Get(int num)
+        {
+            if (IsMultipleOf3(num))
+                return "Fizz";
+
+            else if (IsMultipleOf5(num))
+               return "Fizz";
+
+            else
+                return num.ToString();
+        }
+
+        private static bool IsMultipleOf3(int num)
+        {
+            return (num % 3 == 0);
+                
+        }
+
+        private static bool IsMultipleOf5(int num)
+        {
+            return (num % 5 == 0);
+
+        }
+    }
+}
